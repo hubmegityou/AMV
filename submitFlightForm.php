a196d08a237b1d111b0646246477246d0c35c574
hubmegityou <hubmegityou@users.noreply.github.com>
2017-09-01 10:12:10 +0200

    belbel

--- submitFlightForm.php
+++ submitFlightForm.php
@@ -4,52 +4,50 @@ require 'APIData.php';
 require'GreatCircle.php';
 
 // pobranie danych wpisanych przez uzytkownika-> miejsce wylotu 
-
 $departure=explode(',  ', $_POST['departure']);
 $departureName=$departure[0];
 $departureCity=$departure[1];
 $departureCountry=$departure[2];
 
 //pobranie danych wpisanych przez uzytkownika-> miejsce przylotu
-
 $arrival= explode(',  ', $_POST['arrival']);
 $arrivalName=$arrival[0];
 $arrivalCity=$arrival[1];
 $arrivalCountry=$arrival[2];
 
 //pobranie danych wpisanych przez uzytkownika-> przewoznik
-
 $airlines= explode(',  ',$_POST['airlines']);
 $airlinesName=$airlines[0];
 $airlinesCountry= $airlines[1];
 
-$airport = expole(', ', $_POST['change2']);
-$airportName = $airport[0];
-$airportCity = $airport[1];
-$airport
-//pobranie danych lotnisk, regionów oraz linii lotniczych z API
-
 $APIData= new APIData();
+//pobranie danych lotnisk, regionów oraz linii lotniczych z API
 $curlAirportResult= $APIData->curl($APIData->AirportsUrl);
 $curlAirspacesResult= $APIData->curl($APIData->AirspacesUrl);
 $curlAirlinesResult= $APIData->curl($APIData->AirlinesUrl);
 
-
+//pobranie danych wpisanych przez uzytkownika-> miejsce przesiadki
+if (isset($_POST['change'])){
+    $airport = expole(', ', $_POST['change2']);
+    $airportName = $airport[0];
+    $airportCity = $airport[1];
+    $airportCountry = $airport[2];
+    $airportData = $APIData->FindData($airportCountry, $airportName, $airportCity, $curlAirportResult);
+    $airportRegion = $APIData->FindRegion($airportData, $curlAirportResult);
+    var_dump($airportData);
+    var_dump($airportRegion);
+}
 //pobranie rekordów z danymi wylotu, przylotu oraz linii lotniczych podanych prze uzytkownika
-
 $departureData= $APIData->FindData($departureCountry, $departureName,$departureCity,$curlAirportResult);
 $arrivalData=$APIData->FindData($arrivalCountry, $arrivalName,$arrivalCity,$curlAirportResult);
 $airlinesData=$APIData->FindData( $airlinesCountry, $airlinesName,0, $curlAirlinesResult);
 
 // sprawdzenie regionów lotniska wylotu, przylotu oraz linii lotniczych podanych przez uzytkownika
-
 $departureRegion=$APIData->FindRegion($departureData, $curlAirspacesResult);
 $arrivalRegion= $APIData->FindRegion($arrivalData, $curlAirspacesResult);
 $airlinesRegion= $APIData->FindRegion($airlinesData, $curlAirspacesResult);
 
-
 //weryfikacja czy lot nadaje się do objęcia zgłoszeniem roszczenia
-
 if ($departureRegion!='EUR' && $arrivalRegion!='EUR'){
     echo "lot nie nadaje sie do objecia zgloszeniem roszczenia -  lecisz spoza europy poza europe";
 } 
