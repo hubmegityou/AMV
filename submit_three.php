23cec5d3f81796937fda580d7c7c9b58efb08122
srututututu <ola.stolorz@vp.pl>
2017-09-14 13:51:35 +0200

    submit application

---
+++ submit_three.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+
+$val = $_POST['val'];
+
+
+require_once "database/dbinfo.php";
+require_once "database/connect.php";
+$connection = db_connection();
+
+$sql = "UPDATE  $db_application_tab SET $db_application_delay = '$val' WHERE $db_application_id=".$_SESSION['application'];
+$connection->query($sql);
