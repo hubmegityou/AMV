e3b6334e4d390d1308f7441bcf9cb45a963710f4
hubmegityou <hubmegityou@users.noreply.github.com>
2017-09-20 12:45:27 +0200

    coś

---
+++ newjs.js
@@ -0,0 +1,42 @@
+jQuery(document).ready(function($){
+    Y.addEventListener('click', function(){
+        $('#airports').show();
+    });
+    N.addEventListener('click', function (){
+        $('#airports').hide();
+        $('html,body').animate({scrollTop: $(this).parent().next().offset().top}, time);
+    });
+    $('#step1 > div').hide();
+    var top = $('.topbar');
+    top[1].show();
+    $('#step2').hide();
+    $('#step3').hide();
+    $('#step4').hide();
+    $('#step5').hide();
+    $('airports').show();
+    //move to next question
+    $('img').on('click', function(){
+        var elem = $(this).parent().parent();
+        console.log(elem.length);
+        if (!elem.next().length){
+            var elem = elem.parent();
+        }
+        $('html,body').animate({scrollTop: elem.next().offset().top}, time);
+    })
+    //move to next step
+    $('.btn_next').on('click', function(){
+        var elem = $(this).parent().parent();
+        elem.hide(1000);
+        elem.next().show();
+        $('html,body').animate({scrollTop: elem.offset().top}, time);
+    });
+    //move to previous step
+    $('.btn_prev').on('click', function(){
+        var elem = $(this).parent().parent();
+        elem.hide(500);
+        elem.prev().show(1000);
+        $('html,body').animate({scrollTop: elem.prev().offset().top}, time);        
+    })
+});
+
+var time = 1000;
