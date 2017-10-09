5e218abf3faf2af51da8e2950b50fd38b093436f
Kamil Doleglo <kamilok1965@interia.pl>
2017-10-09 11:49:48 +0200

    Fixed fix

--- Table.class.php
+++ Table.class.php
@@ -54,8 +54,8 @@
             }
         }
         public function update($what, $value){
-            echo("Updating ".$what."\r\n");
-            echo(" with val = ".$value."\r\n");
+           // echo("Updating ".$what."\r\n");
+           // echo(" with val = ".$value."\r\n");
             global $connection;
             
             $sql = "UPDATE `".$this->table_name."` SET `$what` = ? WHERE `".$this->table_id."` = ?";
